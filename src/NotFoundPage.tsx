15bdca11b73065eacb81fde3e6f25d2863d41988
Bill liang <liangbill2006@gmail.com>
2023-03-09 16:01:35 -0500

    refine NotFoundPage and progress on projects page

--- src/NotFoundPage.tsx
+++ src/NotFoundPage.tsx
@@ -1,8 +1,34 @@
+import { NavLink } from "react-router-dom";
+import BackButton from "./BackButton";
+
 function NotFoundPage() {
   return (
-    <div>
-      <h1>404</h1>
-      Not found page.
+    <div className="flex flex-col grow w-[80%] items-center relative text-white mb-10 rounded-md max-w-[1000px] mt-[80px] md:mt-[130px]">
+      <BackButton />
+      <h1 className='text-white text-3xl'>404</h1>
+      <p className="mt-5">Somehow you got here and I'm not even sure how you did it...</p>
+      <p className="mt-5">
+        Please double check the URL to make sure it is correct and try to refresh the page.
+      </p>
+      <p className="mt-5">
+         If you are still seeing this page then whatever you were looking for is no longer there.
+      </p>
+      <p className="mt-5">
+        Why not check out my
+        <NavLink 
+          className="hover:underline hover:cursor-pointer px-1 font-semibold text-gray-500" 
+          to={"/projects"}>
+             projects
+        </NavLink> 
+        or my
+        <NavLink 
+          className="hover:underline hover:cursor-pointer px-1 font-semibold text-gray-500" 
+          to={"/music"}>
+             music
+        </NavLink> 
+        while you are here?
+      </p>
+      
     </div>
   )
 }
--- src/ProjectPage.tsx
+++ src/ProjectPage.tsx
@@ -4,21 +4,18 @@ import BackButton from "./BackButton";
 function ProjectPage() {
 
   return (
-    <div className="flex flex-col grow relative text-white mb-10 rounded-md max-w-[1000px] w-[80%] mt-[80px] md:mt-[130px]">
+    <div className="flex flex-col grow w-[80%] items-center relative text-white mb-10 rounded-md max-w-[1000px] mt-[80px] md:mt-[130px]">
       <BackButton />
-      <p className="mt-5">This is the projects page.</p>
-      <br/>
-      <p>It will show my projects and my entreprenuerial journey.</p>
-      <br/>
-      <p>Still under construction at the moment. 
-        Why not
-        <NavLink 
-          className="hover:underline hover:cursor-pointer px-1 font-semibold text-gray-500" 
-          to={"/music"}>
-            check out my music
-          </NavLink> 
-        while you are here?
+      <h1 className='text-white text-3xl'>Simple ideas that I had fun building</h1>
+      <p className="mt-5">More projects coming soon...</p>
+
+
+      <p>
+        Over the years, I found myself with a strong passion for online education and mental health. 
+        I'd love to hang out with like-minded people and work on exciting new ideas. 
+        Please feel free to send me a message on <a href='https://www.linkedin.com/in/bill-liang' rel='noreferrer' target='_blank' className='hover:underline text-gray-500 font-semibold'>LinkedIn</a>, and start a conversation today!
       </p>
+      
     </div>
   )
 }
